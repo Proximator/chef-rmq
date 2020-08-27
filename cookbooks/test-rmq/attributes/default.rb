158b34ef6bfc06a2d39298f75e5e953dbb731ca6
proximator <mohamed.elgabbouch@gmail.com>
2020-08-27 05:55:52 +0100

    Update default.rb

--- cookbooks/test-rmq/attributes/default.rb
+++ cookbooks/test-rmq/attributes/default.rb
@@ -4,6 +4,6 @@ default['erlang']['source']['version']='R13B03'
 default['erlang']['source']['checksum']='e7c46c8b2778f22064a3b369c1a1b572a1cc0e8a2198166858d4b9a1b488d662'
 
 #RabbitMQ
-default['rabbitmq']['erlang']['enabled'] = true
+default['rabbitmq']['erlang']['enabled'] = false
 default['rabbitmq']['version'] = "3.4.4"
 default['rabbitmq']['rpm_package'] ='rabbitmq-server-3.4.4-1.noarch.rpm'
